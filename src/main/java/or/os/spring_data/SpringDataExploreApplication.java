0455b24ad9aef46b68b9b7a0e0e0eeb82cecec63
Oussama1Belkadi <oussama.belkadi@symolia.com>
2025-04-23 10:02:58 +0100

    Project structure

---
+++ src/main/java/or/os/spring_data/SpringDataExploreApplication.java
@@ -0,0 +1,13 @@
+package or.os.spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataExploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataExploreApplication.class, args);
+    }
+
+}
