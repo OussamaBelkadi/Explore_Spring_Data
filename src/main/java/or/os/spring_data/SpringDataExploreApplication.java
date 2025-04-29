a00be6fdda46cba772405364a179930fedfb1da5
Oussama1Belkadi <oussama.belkadi@symolia.com>
2025-04-29 09:59:52 +0100

    integrate the faker dependency to fake the record in the table, using the @Modifying anotation and @Query to build query can make change in all record in the table

--- src/main/java/or/os/spring_data/SpringDataExploreApplication.java
+++ src/main/java/or/os/spring_data/SpringDataExploreApplication.java
@@ -1,6 +1,9 @@
 package or.os.spring_data;
 
+import com.github.javafaker.Faker;
+import or.os.spring_data.models.Author;
 import or.os.spring_data.models.Video;
+import or.os.spring_data.repository.AuthorRepository;
 import or.os.spring_data.repository.VideoRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,13 +19,19 @@ public class SpringDataExploreApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(VideoRepository repository) {
+    public CommandLineRunner commandLineRunner(AuthorRepository repository) {
         return args -> {
-            Video video = Video.builder()
-                    .title("Os")
-                    .length(12)
-                    .build();
-            repository.save(video);
+            for (int i = 1; i <= 10; i++) {
+                Faker faker = new Faker();
+                Author author = Author.builder()
+                        .firstName(faker.name().firstName())
+                        .lastName(faker.name().lastName())
+                        .age(faker.number().numberBetween(20, 60))
+                        .email(faker.internet().emailAddress())
+                        .build();
+                repository.save(author);
+            }
+            repository.updateAllAuthorsAge(26);
             System.out.println("Spring Data JPA Application");
             // You can add your logic here to test the application
         };
--- src/main/java/or/os/spring_data/repository/AuthorRepository.java
+++ src/main/java/or/os/spring_data/repository/AuthorRepository.java
@@ -1,10 +1,24 @@
 package or.os.spring_data.repository;
 
+import jakarta.transaction.Transactional;
 import or.os.spring_data.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    //   Update list of column value with this query
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age")
+    void updateAllAuthorsAge(int age);
+
     List<Author> findAllByFirstName(String firstName);
+    List<Author> findAllByFirstNameIgnoreCase(String str);
+    List<Author> findAllByFirstNameContainingIgnoreCase(String str);
+    List<Author> findAllByFirstNameStartingWithIgnoreCase(String str);
+    List<Author> findAllByFirstNameEndingWithIgnoreCase(String str);
+
 }
